917d557e75b9680e6e4c9a21365761a0e12262e4
Emad <emadanwer888@gmail.com>
2025-10-01 12:23:31 +0300

    first upload

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+st.title('Covid-19 Data Analysis')
+st.markdown(''' 
+            This app created to analysis Covid-19 data.
+            ''')
+
+st.image('https://www.mdpi.com/ijerph/ijerph-18-02336/article_deploy/html/images/ijerph-18-02336-g002.png',width=200)
+
+st.header('Description')
+st.markdown('''
+            This app created to analysis Covid-19 data.
+            ''')
